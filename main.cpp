f9f2db1830637974d868fbde6e00cd1024e8f4c3
Manvan33 <rikostik@gmail.com>
2022-10-02 16:25:00 +0200

    Etape 5

--- main.cpp
+++ main.cpp
@@ -16,12 +16,23 @@ int main(int argc, const char *argv[]) {
 //    auto *complex = new Multimedia("hello", "/tmp/hello");
 //    std::cout << "Empty class name: " << empty->getName() << std::endl;
 //    std::cout << "Complex class pathname " << complex->getPathname() << std::endl;
-    auto *photo = new Photo("bg.png", "/tmp/", 23.2, 13.1);
+    // Etape 4
+    Photo *photo = new Photo("image.gif", "./", 23.2, 13.1);
     std::cout << "Photo pathname: " << photo->getPathname() << std::endl;
     photo->print(cout);
-    auto *video = new Video("video.mkv", "/tmp/", 4);
+    Video *video = new Video("video.webm", "./", 14);
     video->print(cout);
     photo->play();
     video->play();
+    // Etape 5
+    Multimedia ** tableau = new Multimedia * [2];
+    unsigned int count = 0;
+    tableau[count++] = new Photo("image.gif", "./", 1.0, 2.0);
+    tableau[count++] = new Video("video.webm", "./", 1);
+    for (unsigned int i =0; i < count; i++) {
+        tableau[i]->print(cout);
+        tableau[i]->play();
+    }
+
     return 0;
 }
