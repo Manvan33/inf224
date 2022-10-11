b1b6cef8171501da41c85e40ec1cd802b9ca6de3
Manvan33 <rikostik@gmail.com>
2022-10-11 10:44:58 +0200

    fix: replaced classnames with auto

--- main.cpp
+++ main.cpp
@@ -17,15 +17,15 @@ int main(int argc, const char *argv[]) {
 //    std::cout << "Empty class name: " << empty->getName() << std::endl;
 //    std::cout << "Complex class pathname " << complex->getPathname() << std::endl;
     // Etape 4
-    Photo *photo = new Photo("image.gif", "./", 23.2, 13.1);
+    auto *photo = new Photo("image.gif", "./", 23.2, 13.1);
     std::cout << "Photo pathname: " << photo->getPathname() << std::endl;
     photo->print(cout);
-    Video *video = new Video("video.webm", "./", 14);
+    auto *video = new Video("video.webm", "./", 14);
     video->print(cout);
     photo->play();
     video->play();
     // Etape 5
-    Multimedia ** tableau = new Multimedia * [2];
+    auto ** tableau = new Multimedia * [2];
     unsigned int count = 0;
     tableau[count++] = new Photo("image.gif", "./", 1.0, 2.0);
     tableau[count++] = new Video("video.webm", "./", 1);
