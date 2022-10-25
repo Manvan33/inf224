d18483bc891e95f2db371e7ba08655e7a13f4fb0
Manvan33 <rikostik@gmail.com>
2022-10-25 11:34:57 +0200

    Swing: Etape 1

---
+++ frontend/src/fr/manvan/Main.java
@@ -0,0 +1,12 @@
+package fr.manvan;
+
+// Our app uses Swing UI components
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Create a new window
+        Fenetre fenetre = new Fenetre();
+    }
+}
